37fdf265bb8b36ba89b8a0e653b6f1b90d69d138
Magnus Oscar Støleggen <mgnsoscar@gmail.com>
2024-03-25 09:49:28 +0100

    .

--- src/task1.py
+++ src/task1.py
@@ -23,133 +23,164 @@ class Task1:
         self.C_12 = 0
         self.C_1 = self.C_L + self.C_g1 + self.C_12
         self.C_2 = self.C_L + self.C_g1 + self.C_12
-
-            
-    def V_g2(self, N1: int, N2: int, Vg1: np.ndarray) -> np.ndarray:
-        
-        numerator = (
-            e_ch*(
-            (2*(N1 - 1)*((self.C_g1*Vg1/e_ch))) - 
-            (N1**2 + N2**2) +
-            2
-            )
+    
+    def do_task_a(self):
+        self.stability_diagram_task_a(
+            function_for_Vg1 = self.V_g1_task_a,
+            function_for_Vg2 = self.V_g2_task_a
         )
-        denominator = (
-            2 * (1-N2) * self.C_g2
-        )
-
-        return numerator / denominator
-
-    def V_g1(self, N1: int, N2: int, Vg2: np.ndarray) -> np.ndarray:
-        
-        numerator = (
-            e_ch*(
-                (2*(N2 - 1)*((self.C_g2*Vg2)/e_ch)) - 
-                (N1**2 + N2**2) +
-                2
-            )
+    
+    def do_task_b(self) -> None:
+        self.stability_diagram_task_b(
+            function_for_Vg1 = self.V_g1_task_b,
+            function_for_Vg2 = self.V_g2_task_b
         )
             
+    def V_g2_task_a(self, x: int, y: int, N1: int, N2: int, Vg1: np.ndarray) -> np.ndarray:
+        
+        numerator = e_ch * (
+            (y**2 - N2**2) - (N1**2 - x**2) + ((2 * self.C_g1 * Vg1 * (N1 - x))/e_ch)
+        )
         denominator = (
-            2 * (1-N1) * self.C_g1
+            2 * (y - N2) * self.C_g1
         )
 
         return numerator / denominator
 
-    def V_g2_task_b(self, N1:int, N2:int, Vg2: np.ndarray) -> np.ndarray:
+    def V_g1_task_a(self, x: int, y: int, N1: int, N2: int, Vg2: np.ndarray) -> np.ndarray:
         
-        
-        
-        numerator =(
-            -e_ch * (
-                self.C_1 * (
-                    (2*((self.C_g2*Vg2)/e_ch)) + (N1**2+N2**2) - 2
-                )
-                +
-                self.C_L * (
-                    (-2*((self.C_g2*Vg2)/e_ch)*(N1 - 1)) + (N1 * N2) - 1
-                )
-            )
+        numerator = e_ch * (
+            (N1**2 - x**2) - (y**2 - N2**2) + ((2 *self.C_g2 * Vg2 * (y - N2))/e_ch)
         )
         denominator = (
-            2 * (( (N1 - 1)*self.C_1 ) + ((N2 - 1) * self.C_L ) ) * self.C_g1
+            2 * (N1 - x) * self.C_g1
+        )
+
+        try:
+            return numerator / denominator
+        except:
+            return False
+        
+        return numerator / denominator     
+
+    def V_g2_task_b(self, x: int, y:int, N1:int, N2:int, Vg1: np.ndarray) -> np.ndarray:
+
+        c12 = self.C_L
+        cg1 = self.C_g1
+        cg2 = self.C_g2
+        c2 = self.C_L + c12 + cg2
+        
+        numerator = (e_ch / (2*cg2)) * (
+            2*c12*( N1*N2 + cg1*Vg1*(1/e_ch)*(y-N2) - y ) + c2*(N1**2 + N2**2 - (x**2 + y**2) - (2/e_ch)*cg1*Vg1*(N1-x))
+        )
+        
+        denominator = (
+            c2*(N2-y) + c12*(N1 - x)
         )
         
         return numerator / denominator
+    
+    def V_g1_task_b(self, x: int, y:int, N1:int, N2:int, Vg2: np.ndarray) -> np.ndarray:
         
-    def plot_a(self) -> None:
+        c12 = self.C_L
+        cg1 = self.C_g1
+        cg2 = self.C_g2
+        c2 = self.C_L + c12 + cg1
+        
+        numerator = (e_ch / (2*cg1)) * (
+            2*c12*( N1*N2 + cg2*Vg2*(1/e_ch)*(x-N1) - y ) + c2*(N1**2 + N2**2 - (x**2 + y**2) - (2/e_ch)*cg2*Vg2*(N2-y))
+        )
+        
+        denominator = (
+            c2*(N1-x) + c12*(N2 - y)
+        )
+        
+        return numerator / denominator
+    
+        
+    def stability_diagram_task_a(self, function_for_Vg1, function_for_Vg2) -> None:
         """
         Plot V_g1 vs V_g2 for various (N_1, N_2) configurations.
         """
 
         # Generate values for V_g1 and V_g2
-        V_g1 = np.linspace(0, 1e-2, 100)
-        V_g2 = np.linspace(0, 1e-2, 100)
+        Vg1 = np.linspace(0, 1e-2, 100)
+        Vg2 = np.linspace(0, 1e-2, 100)
 
         # Create a figure
         plt.figure(figsize=(8, 6))
-
+        
         # Define configurations of (N_1, N_2)
-        N1s = [0, 1, 2]
 
-        # Plot V_g1 vs V_g2 for each (N_1, N_2) configuration
-        for N_1 in N1s:
-            for N_2 in N1s:
-                Vg2 = self.V_g2(N_1, N_2, V_g1)
-                Vg1 = self.V_g1(N_1, N_2, Vg2)
+        N1 = 0
+        N2 = 1
+        for x in range(1, 4):
 
-                # Check for valid values and plot
-                if not np.isnan(Vg2[0]):
-                    plt.plot(V_g2, Vg1, label=f"$(N_1,N_2) = ({N_1},{N_2})$")
-
-        # Plot specific configurations
-        two_one = self.V_g1(2, 1, V_g1)
-        two_zero = self.V_g1(0, 1, V_g1)
-        plt.plot(two_one, np.linspace(-0.01, 0.01, 100), label="$(N_1,N_2) = (2,1)$")
-        plt.plot(two_zero, np.linspace(-0.01, 0.01, 100), label="$(N_1,N_2) = (0,1)$")
-
-        # Set plot limits and labels
-        plt.xlim(0, 0.010)
-        plt.ylim(-0.01, 0.01)
+            Vg1_function = function_for_Vg1(x, x, N1, N2, Vg2)
+            Vg2_function = function_for_Vg2(x, x, N2, N1, Vg1)
+                        
+            # Plot Vg2 as function of Vg1
+            plt.plot(Vg2, Vg1_function, label=f"$U({N1},{N2}) = U({x},{x})$")
+            plt.plot(Vg2_function, Vg1, label=f"$U({N2},{N1}) = U({x}, {x})$")
+                
+            N1 += 1
+            N2 += 1
+                
         plt.xlabel('$V_{g1} [V]$')
         plt.ylabel('$V_{g2} [V]$')
-        plt.title('$U(N_1,N_2)=U(1,1)$')
+        
+        plt.title('$U(N_1,N_2)=U(N_x,N_y)$')
+        
         plt.legend()
         plt.grid(False)
         plt.show()
 
-        
-    def plot_b(self) -> None:
+    def stability_diagram_task_b(self, function_for_Vg1, function_for_Vg2) -> None:
         """
         Plot V_g1 vs V_g2 for various (N_1, N_2) configurations.
         """
 
         # Generate values for V_g1 and V_g2
-        V_g1 = np.linspace(-0.01, 10e-3, 100)
-        V_g2 = np.linspace(-0.01, 10e-3, 100)
+        Vg1 = np.linspace(0, 1e-2, 100)
+        Vg2 = np.linspace(0, 1e-2, 100)
 
         # Create a figure
         plt.figure(figsize=(8, 6))
-
+        
         # Define configurations of (N_1, N_2)
-        N1s = [(0, 1), (0, 2), (1, 0), (1, 2), (2, 1), (2, 2)]
 
-        # Plot V_g1 vs V_g2 for each (N_1, N_2) configuration
-        for Ns in N1s:
+        N1 = 0
+        N2 = 0
+        for x in range(1, 2):
             
-            Vg1 = self.V_g2_task_b(Ns[0], Ns[1], V_g2)
 
-            # Check for valid values and plot
-            if not np.isnan(Vg1[0]):
-                plt.plot(V_g2, Vg1, label=f"$(N_1,N_2) = ({Ns[0]},{Ns[1]})$")
-
-
-        # Set plot limits and labels
-        plt.xlim(-0.01, 0.010)
-        plt.ylim(-0.01, 0.01)
-        plt.xlabel('$V_{g2} [V]$')
-        plt.ylabel('$V_{g1} [V]$')
-        plt.title('$U(N_1,N_2)=U(1,1)$')
+            for i in range(0, 3):
+                    
+                for j in range(0, 3):
+                    
+                    if i != j:
+                        try:
+                            Vg1_function = function_for_Vg1(x, x, N1 + i, N2 + j, Vg2)
+                            plt.plot(Vg2, Vg1_function, label=f"$U({N1 + i},{N2 + j}) = U({x},{x})$")
+                        except:
+                            pass
+                        #try: 
+                        #    Vg2_function = function_for_Vg2(x, x, N2 + j, N1 + i, Vg1)
+                        #    plt.plot(Vg2_function, Vg1, label=f"$({N2 + j},{N1 + i}) = ({x}, {x})$")
+                        #except:
+                        #    pass
+                    
+                    
+            N1 += 1
+            N2 += 1
+                
+        plt.xlabel('$V_{g1} [V]$')
+        plt.ylabel('$V_{g2} [V]$')
+        
+        plt.title('$U(N_1,N_2)=U(N_x,N_y)$')
+        
+        plt.xlim(0, 0.01)
+        plt.ylim(0, 0.01)
         plt.legend()
         plt.grid(False)
         plt.show()
@@ -157,4 +188,4 @@ class Task1:
 if __name__ == "__main__":
     
     task1 = Task1()
-    task1.plot_b()
+    task1.do_task_b()
